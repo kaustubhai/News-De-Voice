337c6d558229ab6c25f7756dc47853e81f232e99
kaustubh <kaustubh229@gmail.com>
2020-08-19 00:14:07 +0530

    First build commit

---
+++  build/precache-manifest.ef99857df2893da1b5769993bf6fe508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971f9064210c498d375498376cc4ef2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc744d11e86f7dc28497",
+    "url": "/static/css/main.efee0aab.chunk.css"
+  },
+  {
+    "revision": "3352710700480ef2f7ad",
+    "url": "/static/js/2.c076a580.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c076a580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc744d11e86f7dc28497",
+    "url": "/static/js/main.3a0f3ab8.chunk.js"
+  },
+  {
+    "revision": "ca85c3989b045bc4e3be",
+    "url": "/static/js/runtime-main.6c893ce9.js"
+  }
+]);
